7ec49c64ab5e4f4d258859fb8565761184a0f9a1
Roj James <rojjamescarranza@gmail.com>
2024-01-20 22:22:02 +0800

    update my config files

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,38 +1,45 @@
 return {
-  {
-    "williamboman/mason.nvim",
-      config = function()
-      require("mason").setup()
-      end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = { "lua_ls", "dockerls","golangci_lint_ls","gopls","quick_lint_js","tsserver","vtsls","biome","html","cssls" },
-        })
-      end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.golangci_lint_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.quick_lint_js.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.vtsls.setup({})
-      lspconfig.biome.setup({})
-
-      -- Set keymaps
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"dockerls",
+					"golangci_lint_ls",
+					"gopls",
+					"quick_lint_js",
+					"tsserver",
+					"html",
+					"cssls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+--[[	lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.golangci_lint_ls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.quick_lint_js.setup({ capabilities = capabilities })
+--]]
+			-- Set keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,5 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', {})
+vim.cmd("set number")
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 vim.g.mapleader = " "
