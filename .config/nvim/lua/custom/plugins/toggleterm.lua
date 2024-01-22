5a876b72f8c96f7b0cf788cefc059935f5942bb4
Roj James Carranza <rojjamescarranza@gmail.com>
2024-01-22 16:07:39 +0800

    Moving .config files to parent folder

---
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+      direction = "float",
+      open_mapping = [[<c-\>]],
+      insert_mappings = true,
+      terminal_mappings = true,
+    }
+  },
+}
+
+--https://github.com/akinsho/toggleterm.nvim
